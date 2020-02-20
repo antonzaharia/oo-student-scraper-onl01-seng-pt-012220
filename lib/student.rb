de5c5378067600ea253b59a6fff2017fc3fe7b90
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-20 21:39:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each {|k, v| self.send(("#{k}="), v)}
+    student_hash.each { |k, v| send.self(('#{k}='), v) }
     @@all << self
   end
 
